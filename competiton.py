bb6d52c060f5638cd0f2ca0f041dc86be598fe0d
Peter Barker <pbarker@barker.dropbear.id.au>
2017-08-09 17:53:21 +1000

    Quick refactoring of initial code

--- competiton.py
+++ competiton.py
@@ -25,106 +25,99 @@ pygame.mixer.pre_init(16000, -16, 2, 2048) # setup mixer to avoid sound lag
 pygame.mixer.init()
 pygame.init()
 
+class Competiton():
 
-pygame.mixer.music.load(PythonScriptsLaunchingMP3File)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
+    def __init__(self):
+        self.rover = None
+        self.copter = None
 
-
-print "\nConnection Function Started, Connecting vehicle on : %s" % ttyUSB0
-
-USB0Vehicle = connect(ttyUSB0, wait_ready = True, baud=57600)
-
-print "\nConnection Function Complete"
-
-print "\nttyUSB0's firmware : %s" % USB0Vehicle.version
-
-if USB0Vehicle.version == "APM:Rover-3.1.2"  :
-         print "Rover's DeviceNumber is Correct"    
-    pygame.mixer.music.load(DeviceNumberMP3File)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy(): 
-        pygame.time.Clock().tick(10)
-
-elif USB0Vehicle.version != "APM:Rover-3.1.2"  :
-    print "Rover's DeviceNumber is unmatching"
-
-Rover = USB0Vehicle
-
-pygame.mixer.music.load(SuccessfullyConnectedWithGroundVehicleMP3File)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
-
-
-USB1Vehicle = connect(ttyUSB1, wait_ready = True, baud=57600)
-
-print "USB1 Vehicle Version : %s" % USB1Vehicle.version
-
-if USB1Vehicle.version == "APM:Copter-3.5.0-dev0" :
-    print "Copter's Device Number is Correct"
-    pygame.mixer.music.load(DeviceNumberMP3File)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy(): 
-        pygame.time.Clock().tick(10)
-
-
-elif USB1Vehicle.version != "APM:Copter-3.5.0-dev0" :
-    print "Copter's Device Number is Unmatching"
-
-MyCopter = USB1Vehicle
-
-
-pygame.mixer.music.load(TelemetryMP3File)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
-
-
-while(1) :
-
-    if MyCopter.mode.name == "GUIDED"  :           
-        pygame.mixer.music.load(StartTrackingMP3File)
+    def playmp3(self, filepath):
+        pygame.mixer.music.load(filepath)
         pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy(): 
-            pygame.time.Clock().tick(10)
+        while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
 
-        break
+    def both_connected(self):
+        ...
+
+    def connect_rover(self):
+        USB0Vehicle = connect(ttyUSB0, wait_ready = True, baud=57600)
+        print "\nttyUSB0's firmware : %s" % USB0Vehicle.version
+        if USB0Vehicle.version == "APM:Rover-3.1.2"  :
+                 print "Rover's DeviceNumber is Correct"
+            self.playmp3(DeviceNumberMP3File)
+            self.rover = USB0Vehicle
+
+        elif USB0Vehicle.version != "APM:Rover-3.1.2"  :
+            print "Rover's DeviceNumber is unmatching"
+
+    def connect_copter(self):
+
+        USB1Vehicle = connect(ttyUSB1, wait_ready = True, baud=57600)
+
+        print "USB1 Vehicle Version : %s" % USB1Vehicle.version
+
+        if USB1Vehicle.version == "APM:Copter-3.5.0-dev0" :
+            print "Copter's Device Number is Correct"
+            self.playmp3(DeviceNumberMP3File)
+            self.copter = USB1Vehicle
+
+        elif USB1Vehicle.version != "APM:Copter-3.5.0-dev0" :
+            print "Copter's Device Number is Unmatching"
+
+    def connect_both(self):
+        while True:
+            if (self.rover is not None and
+                self.copter is not None):
+                return
+            print("Waiting for vehicles....")
+            time.sleep(1)
+
+    def do_flight(self):
+        self.playmp3(PythonScriptsLaunchingMP3File)
+
+       while not self.both_connected():
+           self.connect_both()
+
+        Rover = USB0Vehicle
+
+        self.playmp3(SuccessfullyConnectedWithGroundVehicleMP3File)
+
+        MyCopter = USB1Vehicle
+
+        self.playmp3(TelemetryMP3File)
+
+        while(1) :
+
+            if MyCopter.mode.name == "GUIDED"  :  
+                self.playmp3(StartTrackingMP3File)
+                break
 
 
-while(1) :
+        while(1) :
 
-        if MyCopter.mode.name == "GUIDED" :
+            if MyCopter.mode.name == "GUIDED" :
 
-        dest = LocationGlobalRelative(Rover.location.global_frame.lat, Rover.location.global_frame.lon, PYJAltitude)
-        MyCopter.simple_goto(dest)
+                dest = LocationGlobalRelative(Rover.location.global_frame.lat, Rover.location.global_frame.lon, PYJAltitude)
+                MyCopter.simple_goto(dest)
 
+                self.playmp3(GPSConditionMP3File)
 
+                time.sleep(1)
 
-        pygame.mixer.music.load(GPSConditionMP3File)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy(): 
-            pygame.time.Clock().tick(10)
-
-        time.sleep(1)
-
-    if MyCopter.mode.name != "GUIDED" :
-
-        pygame.mixer.music.load(WaitingMP3File)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy(): 
-            pygame.time.Clock().tick(10)
-
-        time.sleep(1)
+            if MyCopter.mode.name != "GUIDED" :
+                self.playmp3(WaitingMP3File)
+                time.sleep(1)
 
 
-print " \n completed "
+        print " \n completed "
 
+        self.playmp3(LandingMP3File)
 
-pygame.mixer.music.load(LandingMP3File)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
+    def run(self):
+        self.do_flight()
+
+competiton = Competition()
+competiton.run()
 
 sys.exit
